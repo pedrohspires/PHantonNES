3bb1e6a4ef1f65edbd20d80f51476978790c2c7d
Pedro Henrique <pedropires2603@gmail.com>
2024-02-02 22:58:16 -0300

    Removendo consoles.log

--- src/core/instructions/brk.ts
+++ src/core/instructions/brk.ts
@@ -12,20 +12,12 @@ const exec = (cpu: cpuType) => {
 
     let pcHex = formatNumber(cpu.pc);
 
-    console.log("Status anterior: " + cpu.p)
-    console.log("PC guardado: " + pcHex);
-    console.log("SP inicial: " + formatNumber(cpu.sp));
-
     cpu.memory[cpu.sp--] = Number("0x" + pcHex.substring(0, 2));
     cpu.memory[cpu.sp--] = Number("0x" + pcHex.substring(2, 4));
     cpu.memory[cpu.sp--] = Number("0b" + cpu.p);
 
     let vetInterrupt = "0x" + formatNumber(cpu.memory[0xffff], 2, 16) + formatNumber(cpu.memory[0xfffe], 2, 16);
 
-    console.log("Vetor interrupção: " + vetInterrupt);
-
     cpu.pc = Number(vetInterrupt);
     setInterruptFlag(cpu);
-
-    console.log("Status depois: " + cpu.p)
 }
