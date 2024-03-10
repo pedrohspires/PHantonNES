68b68e2b006426f8c6597ae7579ea3b6f599c862
Pedro Henrique <pedropires2603@gmail.com>
2024-03-10 10:48:29 -0300

    Correção clock e PC nas instruções de branch

--- src/core/instructions/bmi.ts
+++ src/core/instructions/bmi.ts
@@ -12,7 +12,8 @@ const exec = (cpu: cpuType) => {
     if (negative) {
         let oldPc = cpu.pc;
         cpu.pc += getNumberFromSignedByte(readFirstArgument(cpu));
-        cpu.pc += 2;
-        cpu.clock += (formatNumber(oldPc).substring(0, 2) != formatNumber(cpu.pc).substring(0, 2) ? 5 : 3); // 2 + 1 pelo sucesso + 2 se foi para nova página
+        cpu.clock += (formatNumber(oldPc).substring(0, 2) != formatNumber(cpu.pc).substring(0, 2) ? 3 : 1); // 2 + 1 pelo sucesso + 2 se foi para nova página
     }
+    cpu.clock += 2
+    cpu.pc += 2;
 }
